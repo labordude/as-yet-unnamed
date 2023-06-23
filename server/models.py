0873702e986c2aae21b5543a350b8b9e4da6cd35
Vahan Nadjarian <vahan@nadjarian.com>
2023-06-23 10:37:02 -0400

    game and review models added

--- server/models.py
+++ server/models.py
@@ -1,19 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.associationproxy import association_proxy
 from config import db, bcrypt
+import datetime
 
 
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column()
     _password_hash = db.Column(db.String)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
+    created_at = db.Column(created_at=db.Date, nullable=False)
+    updated_at = db.Column(updated_at=db.Date)
 
-    serialize_rules = ("-password_hash",)
+    reviews = db.relationship('Review', back_populates='user')
+    games = association_proxy('reviews', 'game')
+
+    serialize_rules = ("-password_hash","-created_at","-updated_at")
 
     # @validates("username")
     # def validate_username(self, key, username):
@@ -39,3 +47,39 @@ class User(db.Model, SerializerMixin):
         return f"<User {self.username}"
 
     pass
+
+
+class Game(db.Model, SerializerMixin):
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    background_image = db.Column(db.String)
+    cover_image = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+
+    reviews = db.relationship('Review', back_populates="game")
+    users = association_proxy('reviews', 'user')
+
+    def __repr__(self):
+        return f"<Game {self.title}" 
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(created_at=db.Date, nullable=False)
+    updated_at = db.Column(updated_at=db.Date)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', back_populates='reviews')
+
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
+    game = db.relationship('Game', back_populates='reviews')
+
+    serialize_rules = ("-created_at", "-updated_at")
+   
