8b224588b846bf8cfd88a78d61d9b99d02f464b5
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-06-27 08:39:39 -0500

    adding pipfile dependencies

--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ from flask import Flask, request, session, abort, flash, redirect, url_for
 from flask_migrate import Migrate
 from flask_restful import Resource
 from flask_cors import CORS
+
 # from flask_paginate import Pagination
 from sqlalchemy.exc import IntegrityError
 from config import db, api, app
@@ -22,55 +23,62 @@ from flask_login import login_user
 from flask_login import logout_user
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
+
 # import jsonify
 
 
 class Following(Resource):
     def get(self, id):
-        user_following = followers.query.filter(followers.c.followed_id == id).all()
+        user_following = followers.query.filter(
+            followers.c.followed_id == id
+        ).all()
         if user_following is None:
             return ({"error": "Followings not found"}, 404)
         return user_following.to_dict(), 200
-    
+
+
 api.add_resource(Following, "/api/users/<int:id>/following")
 
-@app.route('/follow/<username>', methods=['POST'])
+
+@app.route("/follow/<username>", methods=["POST"])
 @login_required
 def follow(username):
     form = EmptyForm()
     if form.validate_on_submit():
         user = User.query.filter(User.username == username).first()
         if not user:
-            flash('User {} not found.'.format(username))
-            return redirect(url_for('/api/users/<int:id>'))
+            flash("User {} not found.".format(username))
+            return redirect(url_for("/api/users/<int:id>"))
         if user == current_user:
-            flash('You cannot follow yourself!')
-            return redirect(url_for('/api/users/<int:id>', username=username))
+            flash("You cannot follow yourself!")
+            return redirect(url_for("/api/users/<int:id>", username=username))
         current_user.follow(user)
         db.session.commit()
-        flash('You are following {}!'.format(username))
-        return redirect(url_for('/api/users/<int:id>', username=username))
+        flash("You are following {}!".format(username))
+        return redirect(url_for("/api/users/<int:id>", username=username))
     else:
-        return redirect(url_for('/api/users/<int:id>')) 
+        return redirect(url_for("/api/users/<int:id>"))
 
-@app.route('/unfollow/<username>', methods=['POST'])
+
+@app.route("/unfollow/<username>", methods=["POST"])
 @login_required
 def unfollow(username):
     form = EmptyForm()
     if form.validate_on_submit():
         user = User.query.filter(User.username == username).first()
         if not user:
-            flash('User {} not found.'.format(username))
-            return redirect(url_for('/api/users/<int:id>'))
+            flash("User {} not found.".format(username))
+            return redirect(url_for("/api/users/<int:id>"))
         if user == current_user:
-            flash('You cannot unfollow yourself!')
-            return redirect(url_for('user', username=username))
+            flash("You cannot unfollow yourself!")
+            return redirect(url_for("user", username=username))
         current_user.unfollow(user)
         db.session.commit()
-        flash('You are not following {}.'.format(username))
-        return redirect(url_for('user', username=username))
+        flash("You are not following {}.".format(username))
+        return redirect(url_for("user", username=username))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
+
 
 # app = Flask(
 #     __name__,
@@ -451,7 +459,7 @@ class CommunitiesByID(Resource):
         if not community:
             return ({"error": "Community not found"}, 404)
         return community.to_dict(), 200
-    
+
 
 # add routes for platform games?
 
--- server/config.py
+++ server/config.py
@@ -4,11 +4,12 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 convention = {
@@ -27,5 +28,5 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
-
+login = LoginManager(app)
 api = Api(app)
