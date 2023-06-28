3679cb20ea4cc2f7bdef4f5d1832af8030d1bc34
Vahan Nadjarian <vahan@nadjarian.com>
2023-06-28 13:29:53 -0400

    added link to social page in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -48,9 +48,9 @@ export default function Header({onLogout, user}) {
             <li>
               <Link to="/communities/all">Communities</Link>
             </li>
-            {/* <li>
-              <a>Threads</a>
-            </li> */}
+            <li>
+              <Link to="/social">Social</Link>
+            </li>
           </ul>
         </div>
         <Link
@@ -73,9 +73,9 @@ export default function Header({onLogout, user}) {
                   Communities
                 </Link>
               </li>
-              {/* <li>
-                <a className="text-xl text-white">Threads</a>
-              </li> */}
+              <li>
+                <Link to="/social"  className="text-xl text-white">Social</Link>
+              </li>
             </>
           ) : null}
           {!user ? (
